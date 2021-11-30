f87007e749406e34cef9b0ad6da651fa0d5b0539
Jayden Elliott <jaydene@umich.edu>
2021-11-30 18:42:31 -0500

    shortened strobwe

--- colorstrobe.py
+++ colorstrobe.py
@@ -7,6 +7,6 @@ lights = leds.light_strip()
 while (True):
     hue = random.random()
     lights.all_off()
-    time.sleep(0.18)
+    time.sleep(0.12)
     lights.fill_region_by_name("main", (hue, 0.99, 0.99))
     time.sleep(0.005)
