f35e613cce77cadbc279fc8098477b5d6937c7df
Jayden Elliott <jaydene@umich.edu>
2021-11-30 01:24:38 -0500

    laskdfj

--- goodsnake.py
+++ goodsnake.py
@@ -2,9 +2,8 @@ import leds
 import time
 
 lights = leds.light_strip()
-
+count = 0
 while True:
-    count = 0
     lights.region_fill(count % 81 + 31, count % 81 + 36, (0, 0, 255))
     count = count + 1
     time.sleep(0.1)
