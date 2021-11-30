aa50b54d8c2fea1fc2f96ac0edde07bde159b067
Jayden Elliott <jaydene@umich.edu>
2021-11-30 14:38:54 -0500

    fixed types in regiontesting

--- regiontesting.py
+++ regiontesting.py
@@ -7,6 +7,6 @@ rgb = (0, 0, 255)
 while True:
     start = input("Start: ")
     end = input("End: ")
-    start = lights.loop_region_fill(start, end, rgb, "l")
+    start = lights.loop_region_fill(int(start), int(end), rgb, "l")
     time.sleep(1.5)
     lights.off()
