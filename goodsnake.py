5995a400cd57b3f5a72ef748fb568651c96e9025
Jayden Elliott <jaydene@umich.edu>
2021-11-30 17:41:16 -0500

    fixed out of bounds goodsnake

--- goodsnake.py
+++ goodsnake.py
@@ -6,13 +6,13 @@ count = 0
 
 while True:
     firsts = []
-    firsts.append(lights.ceiling_region_fill(count, count + 3, (0, 1, 1), "r"))
+    firsts.append(lights.ceiling_region_fill(count, count + 3, (0, 0, 0), "r"))
     firsts.append(lights.ceiling_region_fill(
-        count + 43, count + 46, (0.5, 1, 1), "r"))
+        count + 43, count + 46, (0.5, 0.9, 0.9), "r"))
     firsts.append(lights.ceiling_region_fill(
-        count + 22, count + 25, (0.25, 1, 1), "l"))
+        count + 22, count + 25, (0.25, 0.9, 0.9), "l"))
     firsts.append(lights.ceiling_region_fill(
-        count - 22, count - 19, (0.75, 1, 1), "l"))
+        count - 22, count - 19, (0.75, 0.9, 0.9), "l"))
     time.sleep(0.05)
     for i in firsts:
         lights.set_pixel(i, (0, 0, 0))
