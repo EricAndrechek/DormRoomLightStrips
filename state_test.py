01549fce9676cf4d58dc353f3afd0c780d750477
Eric Andrechek <eric@andrechek.com>
2021-11-30 18:33:56 -0500

    debugging region dicts

--- state_test.py
+++ state_test.py
@@ -3,7 +3,7 @@ import leds
 def main(lights):
     # put whatever you want the switch to do here
     # it can call other functions located within this file
-    print(lights.status("main"))
+    print(lights.get_region_dict("main"))
 
 if __name__ == '__main__':
     lights = leds.light_strip()
