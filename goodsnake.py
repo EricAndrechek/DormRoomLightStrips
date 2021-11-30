3468ef9a4c660969bd16f57de84fe15940387bfa
Jayden Elliott <jaydene@umich.edu>
2021-11-30 18:02:29 -0500

    fixed snake

--- goodsnake.py
+++ goodsnake.py
@@ -7,17 +7,17 @@ count = 0
 while True:
     firsts = []
     firsts.append(lights.ceiling_region_fill(
-        count, count + 3, (0, 0.9, 0.9), "r", False))
+        count, count + 4, (0, 0.9, 0.9), "r", False))
     firsts.append(lights.ceiling_region_fill(
-        count + 43, count + 46, (0.5, 0.9, 0.9), "r", False))
+        count + 43, count + 47, (0.5, 0.9, 0.9), "r", False))
     firsts.append(lights.ceiling_region_fill(
-        count + 22, count + 25, (0.25, 0.9, 0.9), "l", False))
+        count + 22, count + 26, (0.25, 0.9, 0.9), "l", False))
     firsts.append(lights.ceiling_region_fill(
-        count - 22, count - 19, (0.75, 0.9, 0.9), "l", False))
+        count - 22, count - 18, (0.75, 0.9, 0.9), "l", False))
     lights.pixels.show()
 
     for i in firsts:
-        lights.set_pixel(i - 1, (0, 0, 0), False)
+        lights.set_pixel(i, (0, 0, 0), False)
     lights.pixels.show()
     time.sleep(0.05)
     count = count + 1
