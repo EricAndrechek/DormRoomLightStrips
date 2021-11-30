37820c9a6e126797ab01ca218c443968cb67890e
Eric Andrechek <eric@andrechek.com>
2021-11-30 18:32:22 -0500

    trying status tracking

--- state_test.py
+++ state_test.py
@@ -3,7 +3,7 @@ import leds
 def main(lights):
     # put whatever you want the switch to do here
     # it can call other functions located within this file
-    print(lights.get_all_states())
+    print(lights.status("main"))
 
 if __name__ == '__main__':
     lights = leds.light_strip()
