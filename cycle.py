21c4590f38e986d7fa08d94973400613c104b494
Jayden Elliott <jaydene@umich.edu>
2021-11-30 04:04:33 -0500

    asdf

--- cycle.py
+++ cycle.py
@@ -9,4 +9,4 @@ while(True):
     rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2])
     lights.loop_region_fill(0, 86, rgb, "r")
     time.sleep(0.01)
-    hsv = (hsv[0] + 1, hsv[1], hsv[2])
+    hsv = (hsv[0] + 0.1, hsv[1], hsv[2])
