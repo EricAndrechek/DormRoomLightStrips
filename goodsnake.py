c9ecbd1ccb61d22fc6e912466e46a6ef621255d2
Jayden Elliott <jaydene@umich.edu>
2021-11-30 15:06:50 -0500

    snake colors

--- goodsnake.py
+++ goodsnake.py
@@ -7,10 +7,13 @@ rgb = (0, 0, 255)
 
 while True:
     firsts = []
-    firsts.append(lights.loop_region_fill(count, count + 3, rgb, "r"))
-    firsts.append(lights.loop_region_fill(count + 43, count + 46, rgb, "r"))
-    firsts.append(lights.loop_region_fill(count + 22, count + 25, rgb, "l"))
-    firsts.append(lights.loop_region_fill(count - 22, count - 19, rgb, "l"))
+    firsts.append(lights.loop_region_fill(count, count + 3, (0, 0, 255), "r"))
+    firsts.append(lights.loop_region_fill(
+        count + 43, count + 46, (50, 255, 0), "r"))
+    firsts.append(lights.loop_region_fill(
+        count + 22, count + 25, (255, 0, 0), "l"))
+    firsts.append(lights.loop_region_fill(
+        count - 22, count - 19, (0, 255, 255), "l"))
     time.sleep(0.05)
     for i in firsts:
         lights.set_pixel(i, (0, 0, 0))
