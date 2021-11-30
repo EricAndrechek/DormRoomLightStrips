abc4b872d6408ee290697e40f28c4ed35c877f53
Jayden Elliott <jaydene@umich.edu>
2021-11-30 16:07:35 -0500

    changed cycle speed

--- cycle.py
+++ cycle.py
@@ -6,6 +6,6 @@ lights = leds.light_strip()
 
 hsv = (0.98, 0.999, 0.999)
 while(True):
-    hsv = (hsv[0] + 0.00005, hsv[1], hsv[2])
+    hsv = (hsv[0] + 0.0001, hsv[1], hsv[2])
     lights.all(hsv)
-    time.sleep(0.1)
+    time.sleep(0.2)
