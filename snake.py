0af2ccd81fc5d66d97a22f4b921199ca43f3d4c3
Eric Andrechek <eric@andrechek.com>
2021-11-30 01:16:36 -0500

    snake code

--- snake.py
+++ snake.py
@@ -1,12 +1,15 @@
 import leds
 import time
+import random
 
 lights = leds.light_strip()
 
 while True:
+    color = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
+    color2 = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
     for i in range(31, 118):
-        lights.set_pixel(i, (0, 0, 255))
+        lights.set_pixel(i, color)
         time.sleep(0.1)
     for i in range(31, 118):
-        lights.set_pixel(i, (0, 0, 0))
+        lights.set_pixel(i, color2)
         time.sleep(0.1)
