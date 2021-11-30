b13b1596b85949982f817ae7a6774068d8ba21bb
Jayden Elliott <jaydene@umich.edu>
2021-11-30 04:11:33 -0500

    asdf

--- leds.py
+++ leds.py
@@ -84,7 +84,7 @@ class light_strip:
     def correct_color(self, rgb):
         hsv = colorsys.rgb_to_hsv(rgb[0]/256, rgb[1]/256, rgb[2]/256)
         hsv = (hsv[0], hsv[1] ** 0.2, hsv[2])
-        rgb = 256 * colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2])
+        rgb = colorsys.hsv_to_rgb(256 * hsv[0], 256 * hsv[1], 256 * hsv[2])
         return rgb
 
     def all_pixels(self, rgb):
