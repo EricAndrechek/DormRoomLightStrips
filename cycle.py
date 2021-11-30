1088d95d4ea5d75abb18da2d7b9293ea0191e6e1
Jayden Elliott <jaydene@umich.edu>
2021-11-30 17:25:02 -0500

    lower brightness

--- cycle.py
+++ cycle.py
@@ -4,7 +4,7 @@ import colorsys
 
 lights = leds.light_strip()
 
-hsv = (0, 0.999, 0.999)
+hsv = (0, 0.999, 0.9)
 while(True):
     hsv = (hsv[0] + 0.001, hsv[1], hsv[2])
     lights.loop_region_fill(0, 87, hsv, "r")
