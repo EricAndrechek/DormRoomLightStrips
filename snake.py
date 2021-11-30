b037287e1c992be5325b4b6f34ea2b933b8dc302
Eric Andrechek <eric@andrechek.com>
2021-11-30 01:03:04 -0500

    snake code

--- snake.py
+++ snake.py
@@ -3,5 +3,7 @@ import time
 
 lights = leds.light_strip()
 
-for i in range(31, 117):
-    
+while True:
+    for i in range(31, 112):
+        lights.region_fill(i, i+5, (0, 0, 255))
+        time.sleep(0.01)
