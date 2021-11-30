3d046221b4173505093b17da9e3e118ad5916e95
Jayden Elliott <jaydene@umich.edu>
2021-11-30 18:35:50 -0500

    fixed wave

--- wave.py
+++ wave.py
@@ -8,7 +8,7 @@ hue = 0
 start = 0
 while True:
     hue = (hue + 0.25 + 0.5 * random.random()) % 1
-    start = (start + random.randrange(22, 66)) % 87
+    start = (start + 22 + round(43 * random.randrange())) % 87
     for i in range(0, 44):
         lights.ceiling_set_pixel(start + i, (hue, 0.99, 0.99), "r", False)
         lights.ceiling_set_pixel(start - i, (hue, 0.99, 0.99), "r", False)
