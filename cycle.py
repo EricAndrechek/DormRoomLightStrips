ced47bef04f0fce4f0cc746aef38a1c615adf9a6
Jayden Elliott <jaydene@umich.edu>
2021-11-30 15:52:47 -0500

    changed cylce

--- cycle.py
+++ cycle.py
@@ -5,10 +5,6 @@ import colorsys
 lights = leds.light_strip()
 
 hsv = (0, 1, 1)
-rgb = (255, 0, 0)
 while(True):
-    """ rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2])
-    rgb = (50 * rgb[0], 50 * rgb[1], 50 * rgb[2])
-    lights.loop_region_fill(0, 86, rgb, "r")
-    time.sleep(0.5)
-    hsv = (hsv[0] + 0.01, hsv[1], hsv[2]) """
+    hsv = (hsv[0] + 0.01, hsv[1], hsv[2])
+    time.sleep(0.1)
