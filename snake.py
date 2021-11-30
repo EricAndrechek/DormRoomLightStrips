63916398dc69b207d9859a75f57ea6b665861a49
Eric Andrechek <eric@andrechek.com>
2021-11-30 01:14:01 -0500

    snake code

--- snake.py
+++ snake.py
@@ -7,4 +7,6 @@ while True:
     for i in range(31, 118):
         lights.set_pixel(i, (0, 0, 255))
         time.sleep(0.1)
-    lights.off()
+    for i in range(31, 118):
+        lights.set_pixel(i, (0, 0, 0))
+        time.sleep(0.1)
