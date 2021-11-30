3aef1ec9075784118b8da6730d717f91827da2a0
Jayden Elliott <jaydene@umich.edu>
2021-11-30 02:33:04 -0500

    asdf

--- goodsnake.py
+++ goodsnake.py
@@ -8,13 +8,13 @@ while True:
     start = 31 + count % 86
     end = start + 5
     if (end > 117):
-        end = end - 86
+        end = end - 86\
     if (end > start and end != 117):
         lights.region_fill(start, end, rgb)
     if (end > start and end == 117):
         lights.region_fill(start, 116, rgb)
         lights.set_pixel(117, rgb)
-    if (end < start):
+    else:
         lights.region_fill(start, 116, rgb)
         lights.set_pixel(117, rgb)
         lights.region_fill(104, end)
