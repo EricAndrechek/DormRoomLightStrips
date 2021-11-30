5011a183baeaebd98de3a3c5c9e83d419a9e61df
Jayden Elliott <jaydene@umich.edu>
2021-11-30 18:25:44 -0500

    added to wave

--- wave.py
+++ wave.py
@@ -11,4 +11,11 @@ while True:
         lights.ceiling_set_pixel(i, (hue, 0.99, 0.99), "r", False)
         lights.ceiling_set_pixel(i, (hue, 0.99, 0.99), "l", False)
         lights.pixels.show()
-        time.sleep(0.1)
+        time.sleep(0.06)
+
+    hue = random.random()
+    for i in range(0, 44):
+        lights.ceiling_set_pixel(44-i, (hue, 0.99, 0.99), "r", False)
+        lights.ceiling_set_pixel(44-i, (hue, 0.99, 0.99), "l", False)
+        lights.pixels.show()
+        time.sleep(0.06)
