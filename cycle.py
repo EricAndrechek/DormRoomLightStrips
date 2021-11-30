90238ef02159c4d020b06638cca3161065a147fa
Jayden Elliott <jaydene@umich.edu>
2021-11-30 16:00:28 -0500

    slowed cycle

--- cycle.py
+++ cycle.py
@@ -6,6 +6,6 @@ lights = leds.light_strip()
 
 hsv = (0, 0.999, 0.999)
 while(True):
-    hsv = (hsv[0] + 0.01, hsv[1], hsv[2])
+    hsv = (hsv[0] + 0.001, hsv[1], hsv[2])
     lights.all(hsv)
     time.sleep(0.1)
