bfcebc7c4a5dde90425a38d3eb3f20ef48219e0c
Jayden Elliott <jaydene@umich.edu>
2021-11-30 18:34:15 -0500

    fixed wave

--- wave.py
+++ wave.py
@@ -6,8 +6,8 @@ lights = leds.light_strip()
 count = 0
 hue = 0
 while True:
-    hue = (hue + random.randrange(0.25, 0.75)) % 1
-    start = (start + round(random.randrange(22, 66))) % 87
+    hue = (hue + 0.25 + 0.5 * random.random()) % 1
+    start = (start + random.randrange(22, 66)) % 87
     for i in range(0, 44):
         lights.ceiling_set_pixel(start + i, (hue, 0.99, 0.99), "r", False)
         lights.ceiling_set_pixel(start - i, (hue, 0.99, 0.99), "r", False)
