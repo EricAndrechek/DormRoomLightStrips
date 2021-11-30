cd09a2a285b4dee0a98cc4bbf2514e50c01276dc
Jayden Elliott <jaydene@umich.edu>
2021-11-30 16:04:36 -0500

    moved cycle start

--- cycle.py
+++ cycle.py
@@ -4,8 +4,8 @@ import colorsys
 
 lights = leds.light_strip()
 
-hsv = (0.9, 0.999, 0.999)
+hsv = (0.98, 0.999, 0.999)
 while(True):
-    hsv = (hsv[0] + 0.001, hsv[1], hsv[2])
+    hsv = (hsv[0] + 0.0001, hsv[1], hsv[2])
     lights.all(hsv)
     time.sleep(0.1)
