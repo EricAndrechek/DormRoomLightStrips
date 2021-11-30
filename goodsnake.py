8ecb5c44412dda8c670536e7d07a1fb99e6da4cb
Jayden Elliott <jaydene@umich.edu>
2021-11-30 14:26:35 -0500

    added print to goodsnake

--- goodsnake.py
+++ goodsnake.py
@@ -8,5 +8,6 @@ rgb = (0, 0, 255)
 while True:
     start = lights.loop_region_fill(count, count + 4, rgb, "l")
     time.sleep(0.05)
+    print(start)
     lights.set_pixel(start, (0, 0, 0))
     count = count + 1
