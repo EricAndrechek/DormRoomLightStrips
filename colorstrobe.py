d3e5794cd77aa19354a02cfdb763b8797e83d0ee
Jayden Elliott <jaydene@umich.edu>
2021-11-30 01:42:35 -0500

    colorstrobe

---
+++ colorstrobe.py
@@ -0,0 +1,21 @@
+import leds
+import time
+import random
+
+lights = leds.light_strip()
+
+for i in range(50):
+    color = (0,0,0)
+    choice = random()
+    rand = random()*255
+    if (choice < 0.33):
+        rand = random()*255
+        color = (rand, 1 - rand, 0)
+    if (choice >= 0.33 and choice < 0.67):
+        color = (rand, 0, 1 - rand)
+    if (choice >= 0.67):
+        color = (0, rand, 1 - rand)
+    lights.off()
+    time.sleep(0.12)
+    lights.region_fill(31, 118, color)
+    time.sleep(0.01)
