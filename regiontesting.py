56f790b385012a0813eda0e8d770a428fff37296
Jayden Elliott <jaydene@umich.edu>
2021-11-30 14:39:59 -0500

    added direction option to regiontesting

--- regiontesting.py
+++ regiontesting.py
@@ -3,10 +3,10 @@ import time
 
 lights = leds.light_strip()
 rgb = (0, 0, 255)
-
+direction = input("Direction: ")
 while True:
     start = input("Start: ")
     end = input("End: ")
-    start = lights.loop_region_fill(int(start), int(end), rgb, "r")
+    start = lights.loop_region_fill(int(start), int(end), rgb, direction)
     time.sleep(1.5)
     lights.off()
