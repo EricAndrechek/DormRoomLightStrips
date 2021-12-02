496a81782ef6aed01c48e061e2d0e2bbc1ac4199
Jayden Elliott <jaydene@umich.edu>
2021-12-01 20:58:06 -0500

    fixed color rotate

--- color_rotate.py
+++ color_rotate.py
@@ -8,7 +8,6 @@ def main(lights):
     while True:
         for i in range(0, 87):
             lights.ceiling_set_pixel(i, ((hue + i / 87) % 1, 0.99, 0.9))
-            time.sleep(0.1)
         lights.update()
         time.sleep(0.1)
         hue = (hue + 1 / 87) % 1
