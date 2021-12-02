749dc9b373675f602e8af88aee1a9686187a4b2c
Jayden Elliott <jaydene@umich.edu>
2021-12-01 21:07:18 -0500

    changed snakes

--- snakes.py
+++ snakes.py
@@ -10,10 +10,11 @@ def main(lights):
             count, count + 4, (0, 0.9, 0.9), "r"))
         firsts.append(lights.ceiling_region_fill(
             count + 43, count + 47, (0.5, 0.9, 0.9), "r"))
-        firsts.append(lights.ceiling_region_fill(
-            count + 22, count + 26, (0.25, 0.9, 0.9), "l"))
-        firsts.append(lights.ceiling_region_fill(
-            count - 22, count - 18, (0.75, 0.9, 0.9), "l"))
+        if (count % 2 == 0):
+            firsts.append(lights.ceiling_region_fill(
+                count + 22, count + 26, (0.25, 0.9, 0.9), "l"))
+            firsts.append(lights.ceiling_region_fill(
+                count - 22, count - 18, (0.75, 0.9, 0.9), "l"))
         for i in firsts:
             lights.set_pixel(i, (0, 0, 0))
         lights.update()
