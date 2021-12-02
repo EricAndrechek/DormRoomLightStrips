41294c2ecf9937d26c259c7d5f3704e411c61848
Jayden Elliott <jaydene@umich.edu>
2021-12-01 20:55:09 -0500

    added color rotate

---
+++ color_rotate.py
@@ -0,0 +1,17 @@
+import time
+import random
+import leds
+
+
+def main(lights):
+    hue = 0
+    while True:
+        for i in range(0, 87):
+            lights.set_pixel(i, ((hue + i / 87) % 1, 0.99, 0.9))
+            time.sleep(0.1)
+        hue = (hue + 1 / 87) % 1
+
+
+if __name__ == '__main__':
+    lights = leds.light_strip()
+    main(lights)
