08650c78e4509cd044ab9978270c8e879a3aa2dd
Jayden Elliott <jaydene@umich.edu>
2021-12-02 01:04:25 -0500

    brightened

--- color_rotate.py
+++ color_rotate.py
@@ -7,7 +7,7 @@ def main(lights):
     hue = 0
     while True:
         for i in range(0, 87):
-            lights.ceiling_set_pixel(i, ((hue + i / 87) % 1, 0.99, 0.7))
+            lights.ceiling_set_pixel(i, ((hue + i / 87) % 1, 0.99, 0.9))
         lights.update()
         time.sleep(0.1)
         hue = (hue + 1 / 87) % 1
