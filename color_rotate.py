8b302595c79ef40e961cf4bed11978183dbb0565
Jayden Elliott <jaydene@umich.edu>
2021-12-02 02:59:43 -0500

    fixed color rotate speed

--- color_rotate.py
+++ color_rotate.py
@@ -8,16 +8,12 @@ def main(lights, speed=0.1):
     while True:
         i = 0
         while i < 87:
-            num_at_time = 1
-            if speed < 0.001:
-                num_at_time = 5
-            for j in range(0, num_at_time):
-                lights.ceiling_set_pixel(
-                    i + j, ((hue + (i + j) / 87) % 1, 0.99, 0.9))
-            i = i + num_at_time
+            lights.ceiling_set_pixel(i, ((hue + i / 87) % 1, 0.99, 0.9))
+            i = i + 1
         lights.update()
-        time.sleep(speed)
-        hue = (hue + 1 / 87) % 1
+        time.sleep(speed if speed >= 0.001 else 0.001)
+        num_at_time = 1 if speed >= 0.001 else 2
+        hue = (hue + num_at_time / 87) % 1
 
 
 if __name__ == '__main__':
