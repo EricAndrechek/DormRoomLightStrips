71dfca0e8f43782cf5f1fa31998bd8432c9fd227
Jayden Elliott <jaydene@umich.edu>
2021-12-03 18:32:59 -0500

    shorter breaks

--- spotify_lights.py
+++ spotify_lights.py
@@ -100,7 +100,7 @@ def wave2(lights, beat, start_time, duration, min_loudness, max_loudness, hue_sh
         lights.update()
         time.sleep(duration / 30)
     active_beats.append([0, hsv, length, 0])
-    for i in range(0, 9):
+    for i in range(0, 5):
         update_active_beats()
         lights.update()
         time.sleep(duration / 30)
