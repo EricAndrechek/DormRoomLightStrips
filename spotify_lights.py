04278e978349998dbbbfd2042cfa84747381fadb
Jayden Elliott <jaydene@umich.edu>
2021-12-03 02:57:27 -0500

    optional hue adjust

--- spotify_lights.py
+++ spotify_lights.py
@@ -71,23 +71,25 @@ def wave(lights, beat, start_time, min_loudness, max_loudness, hue_shift):
 
 
 def main(lights):
-    last_hsv = (0, 0, 0)
+    adjust_hue = False
     last_url = ""
     while True:
-        url = ""
-        album_hue = 0
-        if spotify.is_playing():
-            url = spotify.get_album_image()
-            if url is not None and url != "" and url != last_url:
-                last_url = url
-                print(url)
-                image_bytes = BytesIO(urllib.request.urlopen(url).read())
-                image = np.array(Image.open(image_bytes))
-                helper = image_color_helper.SpotifyBackgroundColor(
-                    image, image_processing_size=(100, 100))
-                new_color = helper.best_color()
-                hsv = lights.rgb_to_hsv(new_color)
-                album_hue = hsv[0]
+            url = ""
+            album_hue = 0
+            if spotify.is_playing():
+                if adjust_hue:
+                    url = spotify.get_album_image()
+                    if url is not None and url != "" and url != last_url:
+                        last_url = url
+                        print(url)
+                        image_bytes = BytesIO(
+                            urllib.request.urlopen(url).read())
+                        image = np.array(Image.open(image_bytes))
+                        helper = image_color_helper.SpotifyBackgroundColor(
+                            image, image_processing_size=(100, 100))
+                        new_color = helper.best_color()
+                        hsv = lights.rgb_to_hsv(new_color)
+                        album_hue = hsv[0]
 
             track = spotify.get_audio_features()[0]["id"]
             min_loudness = 0
@@ -102,12 +104,15 @@ def main(lights):
                     max_loudness = beat["loudness"]
                 hues.append(beat["pitch"])
             avg_hue = circular_average(hues)
-            hue_shift = album_hue - avg_hue
-            position = 0
-            start_time = time.time() + get_playback_position() + 0.5
+            if adjust_hue:
+                hue_shift = album_hue - avg_hue]
+            else:
+                hue_shift= 0
+            position= 0
+            start_time= time.time() + get_playback_position() + 0.5
             for beat in beats:
                 if time.time() + beat["start"] < start_time + position:
-                    position = position + beat["duration"]
+                    position= position + beat["duration"]
                     continue
                 elif time.time() + beat["start"] > start_time + position:
                     time.sleep(time.time() +
