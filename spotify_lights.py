5f9dfb9ac6127eddbd691ec89c65f3102803d98e
Jayden Elliott <jaydene@umich.edu>
2021-12-06 01:24:38 -0500

    fixed range

--- spotify_lights.py
+++ spotify_lights.py
@@ -9,7 +9,7 @@ from io import BytesIO
 import urllib.request
 from PIL import Image
 from cmath import sin, cos, phase, pi
-from random import randrange
+import random
 
 
 def circular_average(inputs):
@@ -120,14 +120,10 @@ def pattern3(lights, beat, start_time, duration, min_loudness, max_loudness, hue
     hsv = ((beat["pitch"] + hue_shift) % 1, 0.99, 0.99)
     length = 1 + int(loudness * 5)
     if beat["start"] == 0:
-        center = randrange(0, 87)
+        center = random.randrange(0, 87)
     else:
-        try:
-            center = randrange(
-                prev_beat[0] + prev_beat[1] + length + 1, 87 - prev_beat[0] - prev_beat[1] - length)
-        except ValueError:
-            center = randrange(
-                87 - prev_beat[0] - prev_beat[1] - length - 1, prev_beat[0] + prev_beat[1] + length)
+        center = random.choice(range(0, prev_beat[0] + prev_beat[1] + length + 1) + range(
+            87 - prev_beat[0] - prev_beat[1] - length + 1, 87))
     lights.ceiling_region_fill(0, 87, (0, 0, 0))
     for i in range(0, length + 1):
         lights.ceiling_set_pixel(center + i, hsv)
