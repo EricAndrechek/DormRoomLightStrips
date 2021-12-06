753f67b0c8eaff94b33a1371123c368407231957
Jayden Elliott <jaydene@umich.edu>
2021-12-05 23:48:58 -0500

    added cast

--- spotify_lights.py
+++ spotify_lights.py
@@ -207,7 +207,7 @@ def main(lights):
                     print(duration)
                     if duration > 0:
                         light_pattern(lights, beat, start_time, duration,
-                                      min_loudness, max_loudness, hue_shift, str(pattern))
+                                      min_loudness, max_loudness, hue_shift, int(pattern))
                     else:
                         print("skip")
                     index = index + 1
