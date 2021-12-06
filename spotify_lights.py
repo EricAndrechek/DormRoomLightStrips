b3e6d17e33d7a02d48c0f634f7e57e08c79ac90f
Jayden Elliott <jaydene@umich.edu>
2021-12-06 02:13:44 -0500

    indent

--- spotify_lights.py
+++ spotify_lights.py
@@ -141,8 +141,8 @@ def pattern3(lights, beat, start_time, duration, min_loudness, max_loudness, hue
         for i in range(1, 6):
             lights.ceiling_region_fill(prev_beat[0] - prev_beat[1] - i, prev_beat[0] +
                                        prev_beat[1] + i + 1, (prev_beat[2][0], prev_beat[2][1], 1 - i * 0.2))
-        lights.update()
-        time.sleep(duration / 15)
+            lights.update()
+            time.sleep(duration / 15)
     prev_beat = (center, length, hsv)
 
 
