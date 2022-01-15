d6222360431adcdc363d51976ca6993295db9401
Eric Andrechek <eric@andrechek.com>
2022-01-15 02:05:38 -0500

    WHERE IS THE TUPLE

--- janky_arduino.py
+++ janky_arduino.py
@@ -20,8 +20,8 @@ class connection:
             print("Failed to push data: " + line)
             self.ser.reset_input_buffer()
     def set_pixel(self, pixel, gbr):
-        print(pixel)
         g, b, r = gbr
+        print(pixel, g, b, r)
         self.ser.write("{}\n".format(self.get_char_map(pixel, g, b, r)).encode('utf-8'))
     def get_char_map(n, g, b, r):
         # 32 -> 117 inclusively
