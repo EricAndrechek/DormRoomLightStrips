f58da4af5917bd7646e4842337337cadcadf74b8
Eric Andrechek <eric@andrechek.com>
2022-01-15 00:38:32 -0500

    testing success right

--- tools/arduino_test.py
+++ tools/arduino_test.py
@@ -6,8 +6,10 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
 
-    while True:
+    successful = 0
+    for i in range(0, 118):
         ser.write(b"comm\n")
         line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+        if line == "good":
+            successful += 1
+    print(successful/118 * 100)
