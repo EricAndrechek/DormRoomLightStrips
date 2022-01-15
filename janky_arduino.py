cbb1725526b1ca4489e3abe1b6bd97f0ca95acac
Eric Andrechek <eric@andrechek.com>
2022-01-15 03:18:20 -0500

    check for data differences

--- janky_arduino.py
+++ janky_arduino.py
@@ -76,8 +76,9 @@ class connection:
         to_write = "{}\n".format(char_map).encode('utf-8')
         self.ser.write(to_write)
         line = self.ser.readline().decode('utf-8').rstrip()
-        if line != "{}: {} {} {} {}".format(char_map, pixel, g, b, r):
-            print("Failed to push data: {}, received: {}".format(to_write, line))
+        expected = "{}: {} {} {} {}".format(char_map, pixel, g, b, r)
+        if line != expected:
+            print("Failed to push data: {}, received: {}, expected: {}".format(char_map, line, expected))
             self.ser.reset_input_buffer()
     
     
