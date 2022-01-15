48bab1e8e7b7cb1a5e8e851a4470fc6572807744
Eric Andrechek <eric@andrechek.com>
2022-01-15 00:27:07 -0500

    fixed arduino test code

--- tools/arduino_test.py
+++ tools/arduino_test.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+        ser.write(b"comm\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
