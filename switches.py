65dde4b2910be01b90b0993e5bd2b956c322c2dc
Eric Andrechek <eric@andrechek.com>
2022-01-23 02:55:10 -0500

    adding cli

--- switches.py
+++ switches.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 import click
 import json
@@ -5,19 +6,56 @@ import json
 from nbformat import write
 
 @click.group()
-def cli():
+@click.option('--dry-run', help='Run the code without actually executing it', is_flag=True, default=False)
+@click.pass_context
+def cli(ctx, dry_run):
+    ctx.ensure_object(dict)
+
+    ctx.obj['dry_run'] = dry_run
+
+
+@cli.command()
+@click.option('--search', help='Search for a switch by name or description')
+def list(search):
+    # if search is not none then look for matching text in the name or description of switches.json file and pretty print the results
+    # otherwise just print the switches.txt file
+    if search is not None:
+        write_buffer = ""
+        with open('switches.json') as switches:
+            switches = json.load(switches)
+            for switch in switches:
+                if search.lower() in switch['name'].lower() or search.lower() in switch['description'].lower():
+                    write_buffer += "Switch name: " + switch["name"] + "\n"
+                    write_buffer += "Description: " + switch["description"] + "\n"
+                    write_buffer += "File name: " + switch["internal_name"] + ".py\n"
+                    if switch["is_brightness_slider"]:
+                        write_buffer += "Brightness slider\n"
+                        write_buffer += "Max brightness: " + str(switch["brightness_slider_max"]) + "\n"
+                    if switch["is_rgb"]:
+                        write_buffer += "RGB slider\n"
+                    write_buffer += "\n"
+        if write_buffer == "":
+            print("No switches found matching the search term")
+        else:
+            print(write_buffer)
+    else:
+        with open('switches.txt') as switches:
+            print(switches.read())
+
+
+@cli.command()
+@click.pass_context
+@click.option('--dry-run', help='Run the code without actually executing it', is_flag=True, default=False)
+def build_server(ctx, dry_run):
+    dry_run = dry_run or ctx.obj['dry_run']
     pass
 
 
 @cli.command()
-@cli.option('--dry-run', help='Run the code without actually executing it', is_flag=True)
-def build_server(dry_run):
-    pass
-
-
-@cli.command()
-@cli.option('--dry-run', help='Run the code without actually executing it', is_flag=True)
-def build_devices(dry_run):
+@click.pass_context
+@click.option('--dry-run', help='Run the code without actually executing it', is_flag=True, default=False)
+def build_devices(ctx, dry_run):
+    dry_run = dry_run or ctx.obj['dry_run']
     # open switches.json
     with open('switches.json') as f:
         switches = json.load(f)
@@ -26,26 +64,25 @@ def build_devices(dry_run):
     with open("devices.json", "r") as f:
         devices = json.load(f)
 
-    new_devices = {}
+    new_devices = []
     for switch in switches:
-        new_switch_device = {"accessory": "HttpPushRgb", "service": "Light", "switch": {}}
-        new_switch_device["name"] = switch["name"]
+        new_switch_device = {"accessory": "HttpPushRgb", "name": switch["name"], "service": "Light", "switch": {}}
         new_switch_device["switch"]["notificationID"] = switch["internal_name"]
         new_switch_device["switch"]["status"] = "http://192.168.2.97:8000/status?r=" + switch["internal_name"]
         new_switch_device["switch"]["powerOn"] = "http://192.168.2.97:8000/on?r=" + switch["internal_name"]
         new_switch_device["switch"]["powerOff"] = "http://192.168.2.97:8000/off?r=" + switch["internal_name"]
 
         if switch["is_brightness_slider"]:
-            new_switch_device["switch"]["brightness"] = {}
-            new_switch_device["switch"]["brightness"]["max"] = switch["brightness_slider_max"]
-            new_switch_device["switch"]["brightness"]["status"] = "http://192.168.2.97:8000/bright?r=" + switch["internal_name"]
-            new_switch_device["switch"]["brightness"]["url"] = "http://192.168.2.97:8000/bset/%s?r=" + switch["internal_name"]
+            new_switch_device["brightness"] = {}
+            new_switch_device["brightness"]["max"] = switch["brightness_slider_max"]
+            new_switch_device["brightness"]["status"] = "http://192.168.2.97:8000/bright?r=" + switch["internal_name"]
+            new_switch_device["brightness"]["url"] = "http://192.168.2.97:8000/bset/%s?r=" + switch["internal_name"]
         
         if switch["is_rgb"]:
-            new_switch_device["switch"]["color"] = {}
-            new_switch_device["switch"]["color"]["brightness"] = False
-            new_switch_device["switch"]["color"]["status"] = "http://192.168.2.97:8000/color?r=" + switch["internal_name"]
-            new_switch_device["switch"]["color"]["url"] = "http://192.168.2.97:8000/cset/%s?r=" + switch["internal_name"]
+            new_switch_device["color"] = {}
+            new_switch_device["color"]["brightness"] = False
+            new_switch_device["color"]["status"] = "http://192.168.2.97:8000/color?r=" + switch["internal_name"]
+            new_switch_device["color"]["url"] = "http://192.168.2.97:8000/cset/%s?r=" + switch["internal_name"]
         
         new_devices.append(new_switch_device)
     
@@ -58,17 +95,19 @@ def build_devices(dry_run):
 
     if not dry_run:
         with open("devices.json", "w") as f:
-            json.dump(devices, f)
+            json.dump(devices, f, indent=4)
     else:
         with open("devices-staging.json", "w") as f:
-            json.dump(devices, f)
+            json.dump(devices, f, indent=4)
         print("Wrote devices-staging.json")
         
 
 
 @cli.command()
-@cli.option('--dry-run', help='Run the code without actually executing it', is_flag=True)
-def compile(dry_run):
+@click.pass_context
+@click.option('--dry-run', help='Run the code without actually executing it', is_flag=True, default=False)
+def compile(ctx, dry_run):
+    dry_run = dry_run or ctx.obj['dry_run']
     # read all files in switches folder and parse their first 6 lines
     files = os.listdir("switches")
     switches = []
@@ -76,17 +115,18 @@ def compile(dry_run):
         if file.endswith(".py"):
             with open("switches/" + file, "r") as f:
                 # get the first 5 lines
-                lines = f.readlines()[0:5]
+                lines = f.readlines()[0:6]
                 # parse the lines
                 name = lines[0].split(":")[1].split('#')[0].strip()
                 internal_name = lines[1].split(":")[1].split('#')[0].strip()
                 is_brightness_slider = lines[2].split(":")[1].split('#')[0].strip()
                 is_brightness_slider = is_brightness_slider == "True"
                 brightness_slider_max = int(lines[3].split(":")[1].split('#')[0].strip())
-                is_rgb = lines[3].split(":")[1].split('#')[0].strip()
+                is_rgb = lines[4].split(":")[1].split('#')[0].strip()
                 is_rgb = is_rgb == "True"
-                description = lines[4].split(":")[1].split('#')[0].strip()
-                switches.append({"name": name, "internal_name": internal_name, "is_brightness_slider": is_brightness_slider, "brightness_slider_max": brightness_slider_max, "is_rgb": is_rgb, "description": description})
+                description = lines[5].split(":")[1].split('#')[0].strip()
+                if name != "Example":
+                    switches.append({"name": name, "internal_name": internal_name, "is_brightness_slider": is_brightness_slider, "brightness_slider_max": brightness_slider_max, "is_rgb": is_rgb, "description": description})
 
     if dry_run:
         # print the switches
@@ -94,7 +134,7 @@ def compile(dry_run):
             print("Friendly name: " + switch["name"])
             print("Internal name: " + switch["internal_name"])
             print("Brightness slider: " + str(switch["is_brightness_slider"]))
-            print("Brightness slider max: " + switch["brightness_slider_max"])
+            print("Brightness slider max: " + str(switch["brightness_slider_max"]))
             print("RGB: " + str(switch["is_rgb"]))
             print("Description: " + switch["description"])
             print("")
@@ -116,8 +156,8 @@ def compile(dry_run):
         
         # save the switches to switches.json
         with open("switches.json", "w") as f:
-            json.dump(switches, f)
+            json.dump(switches, f, indent=4)
 
 
 if __name__ == '__main__':
-    cli()
+    cli(obj={})
