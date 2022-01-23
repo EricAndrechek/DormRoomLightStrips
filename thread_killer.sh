2140d8ad84afaba297eafaca6f670b7b1c8c8a19
Eric Andrechek <eric@andrechek.com>
2022-01-23 17:48:45 -0500

    plz just work awk

--- thread_killer.sh
+++ thread_killer.sh
@@ -2,7 +2,7 @@
 
 
 
-sudo pgrep -fl python3 | awk -v pid1=$1 -v pid2=$2 '!/pid1/{print $1}' | awk '!/pid2/{print $1}' | sudo xargs kill -9
+sudo pgrep -fl python3 | awk '!/ARGV[1]/{print $1}' | awk '!/ARGV[2]/{print $1}' | sudo xargs kill -9
 
 SESSION_NAME='switch'
 sudo screen -ls "$SESSION_NAME" | (
