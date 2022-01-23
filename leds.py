2fe5f8f2ae5ffcd63c1d4f7c70480521c87bfbdf
Eric Andrechek <eric@andrechek.com>
2022-01-23 05:18:54 -0500

    thread kill fix

--- leds.py
+++ leds.py
@@ -406,9 +406,10 @@ class light_strip:
         """
         thread: a threading.Thread object
         """
-        thread_id = thread.ident
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, ctypes.py_object(SystemExit))
-        if res > 1:
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
-            print('Exception raise failure')
+        if thread is not None:
+            thread_id = thread.ident
+            res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, ctypes.py_object(SystemExit))
+            if res > 1:
+                ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+                print('Exception raise failure')
 
