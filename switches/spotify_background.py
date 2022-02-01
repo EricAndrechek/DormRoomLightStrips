94922bfb79e0b3f3476eab647dea29d179c32c15
Eric Andrechek <eric@andrechek.com>
2022-02-01 11:38:24 -0500

    push color update at each new song, not album

--- switches/spotify_background.py
+++ switches/spotify_background.py
@@ -13,17 +13,17 @@ import time
 
 def main(lights, brightness=False, rgb=False, spotify=False):
     last_hsv = (0, 0, 0)
-    last_url = ""
+    last_track = ""
     lights.log.debug("spotify_background is now running")
     while not lights.thread_kill:
-        new_url = spotify.get_album_image()
-        if new_url != last_url and spotify.is_playing():
+        new_track = spotify.get_track_id()
+        if new_track != last_track and spotify.is_playing():
             new_hsv = spotify.get_color()
             if new_hsv != last_hsv:
                 lights.log.debug("spotify_background color update: {} - hsv: {}".format(spotify.get_track_title(), new_hsv))
                 lights.smooth_transition(0, 87, last_hsv, new_hsv, 0.3)
                 last_hsv = new_hsv
-            last_url = new_url
+            last_track = new_track
         if not spotify.is_playing():
             if last_hsv != (0, 0, 0):
                 lights.log.debug("spotify_background color update: Nothing playing - hsv: (0, 0, 0)")
