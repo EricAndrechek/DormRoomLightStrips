12d4397680db3942a0062e395782c20badc8f21a
Eric Andrechek <eric@andrechek.com>
2022-02-05 01:32:27 -0500

    reformatted foreground color styling

--- switches/spotify_background.py
+++ switches/spotify_background.py
@@ -25,7 +25,7 @@ def main(lights, brightness=False, rgb=False, spotify=False):
             g = int(g)
             r = int(r)
             b = int(b)
-            hsv_text_block = "HSV: " + str(new_hsv) + " - RGB: (" + str(r) + ", " + str(g) + ", " + str(b) + ") - " + bg(r, g, b) + fg.white + "  COLOR  " + rs.bg + rs.fg
+            hsv_text_block = fg.white + "HSV: " + str(new_hsv) + " - RGB: (" + str(r) + ", " + str(g) + ", " + str(b) + ") - " + bg(r, g, b) + "  COLOR  " + rs.all
             lights.log.info("spotify_background: {} - {}".format(spotify.get_track_title(), hsv_text_block))
             lights.smooth_transition(0, 87, last_hsv, new_hsv, 0.3)
             last_hsv = new_hsv
