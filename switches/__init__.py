8996d8618518079ca593203cf88d652b3bb19eba
Eric Andrechek <eric@andrechek.com>
2022-02-05 03:58:24 -0500

    rebuild devices.json for smaller spotify slider

--- switches/__init__.py
+++ switches/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["color_cycle", "color_overlap", "color_strobe", "random_wave", "umich_colors", "strobe", "spotify_lights_tatum", "color_rotate", "spotify_background", "spotify_lights_beat", "snakes"]
+__all__ = ["color_cycle", "color_overlap", "color_strobe", "random_wave", "umich_colors", "strobe", "spotify_lights_beat", "color_rotate", "spotify_background", "spotify_lights_beat", "snakes"]
